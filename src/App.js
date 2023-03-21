39be3e96ada80e722fdc0e85c3641e490472fbd6
julia <julichis@mail.ru>
2023-03-21 19:31:58 +0400

    header nav

--- src/App.js
+++ src/App.js
@@ -9,65 +9,67 @@ import gaming from './assets/images/image-gaming-growth.jpg';
 function App() {
   return (
     <div>
-      <header>
-        <a href='#' className="logo"><img src={logo} alt="logo" /></a>
-        <nav>
-          <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">New</a></li>
-            <li><a href="#">Popular</a></li>
-            <li><a href="#">Trending</a></li>
-            <li><a href="#">Categories</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-        <section>
-          <article>
-            <img src={img} alt="web 3.0 illustration" />
-            <h1>The Bright Future of Web 3.0?</h1>
-            <p>  We dive into the next evolution of the web that claims to the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
-            <a href="#">Read more</a>
-          </article>
-          <div>
-            <h2>New</h2>
-            <ul>
-              <li>
-                <h3>Hydrogen VS Electric Cars</h3>
-                <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
-              </li>
-              <li>
-                <h3>The Downsides of AI Artistry</h3>
-                <p>What are the possible adverse effects of on-demand AI image generation?</p>
-              </li>
-              <li>
-                <h3>Is VC Funding Drying Up?</h3>
-                <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
-              </li>
+      <div className="wrapper">
+        <header className='header'>
+          <a href='#' className="header__logo"><img src={logo} alt="logo" /></a>
+          <nav>
+            <ul className='header__nav'>
+              <li className='header__nav-item'><a href="#">Home</a></li>
+              <li className='header__nav-item'><a href="#">New</a></li>
+              <li className='header__nav-item'><a href="#">Popular</a></li>
+              <li className='header__nav-item'><a href="#">Trending</a></li>
+              <li className='header__nav-item'><a href="#">Categories</a></li>
             </ul>
-          </div>
-        </section>
-        <section>
-          <article>
-            <img src={retro} alt="retro pcs" />
-            <div>01</div>
-            <h3>Reviving Retro PCs</h3>
-            <p>What happens when old PCs are given modern upgrades?</p>
-          </article>
-          <article>
-            <img src={laptops} alt="top 10 laptops" />
-            <div>02</div>
-            <h3>Top 10 Laptops of 2022</h3>
-            <p>Our best picks for various needs and budgets.</p>
-          </article>
-          <article>
-            <img src={gaming} alt="gaming growth" />
-            <div>03</div>
-            <h3>The Growth of Gaming</h3>
-            <p>How the pandemic has sparked fresh opportunities.</p>
-          </article>
-        </section>
-      </main>
+          </nav>
+        </header>
+        <main>
+          <section>
+            <article>
+              {/* <img src={img} alt="web 3.0 illustration" /> */}
+              <h1>The Bright Future of Web 3.0?</h1>
+              <p>  We dive into the next evolution of the web that claims to the power of the platforms back into the hands of the people. But is it really fulfilling its promise?</p>
+              <a href="#">Read more</a>
+            </article>
+            <div>
+              <h2>New</h2>
+              <ul>
+                <li>
+                  <h3>Hydrogen VS Electric Cars</h3>
+                  <p>Will hydrogen-fueled cars ever catch up to EVs?</p>
+                </li>
+                <li>
+                  <h3>The Downsides of AI Artistry</h3>
+                  <p>What are the possible adverse effects of on-demand AI image generation?</p>
+                </li>
+                <li>
+                  <h3>Is VC Funding Drying Up?</h3>
+                  <p>Private funding by VC firms is down 50% YOY. We take a look at what that means.</p>
+                </li>
+              </ul>
+            </div>
+          </section>
+          <section>
+            <article>
+              <img src={retro} alt="retro pcs" />
+              <div>01</div>
+              <h3>Reviving Retro PCs</h3>
+              <p>What happens when old PCs are given modern upgrades?</p>
+            </article>
+            <article>
+              <img src={laptops} alt="top 10 laptops" />
+              <div>02</div>
+              <h3>Top 10 Laptops of 2022</h3>
+              <p>Our best picks for various needs and budgets.</p>
+            </article>
+            <article>
+              <img src={gaming} alt="gaming growth" />
+              <div>03</div>
+              <h3>The Growth of Gaming</h3>
+              <p>How the pandemic has sparked fresh opportunities.</p>
+            </article>
+          </section>
+        </main>
+      </div>
     </div>
   );
 }
